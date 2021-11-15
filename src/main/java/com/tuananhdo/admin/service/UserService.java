bb311d4d50b2f3b93a1ee10827aa094e7487ac64
Tuan Anh Do <slodierqwe@gmail.com>
2021-11-15 10:20:07 +0700

    Add Service Layer User

---
+++ src/main/java/com/tuananhdo/admin/service/UserService.java
@@ -0,0 +1,19 @@
+package com.tuananhdo.admin.service;
+
+import com.tuananhdo.entity.User;
+import com.tuananhdo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAllUsers(){
+        return (List<User>) userRepository.findAll();
+    }
+}
--- src/test/java/com/tuananhdo/MyprojectApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.tuananhdo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MyprojectApplicationTests {
-
-}
